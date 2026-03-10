ae73758db2b0ed8eed28544f9086493b598591b3
MIDHUN-11 <midhunsubramania@gmail.com>
2026-03-10 20:02:06 +0530

    updated main with additional response

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
+
+OLLAMA_URL = "http://localhost:11434/api/generate"
 
 @app.get("/")
 def home():
     return {"message": "AI Router Running"}
+
+@app.post("/ai")
+def ask_ai(prompt: str):
+
+    payload = {
+        "model": "llama3",
+        "prompt": prompt,
+        "stream": False
+    }
+
+    response = requests.post(OLLAMA_URL, json=payload)
+
+    return response.json()
